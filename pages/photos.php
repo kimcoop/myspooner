4d3dab241e5bcd55ed7dcb0aa07b7a6b74beca67
Kim <kim.cooperrider@gmail.com>
2011-12-26 00:43:33 -0500

    images

--- pages/photos.php
+++ pages/photos.php
@@ -14,28 +14,46 @@ $(function() {
 
 	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?id=72827301@N02&lang=en-us&format=json&jsoncallback=?", function(data){
 			$.each(data.items, function(i,item){
-				$("<img/>").attr("src", item.media.m).appendTo("#images")
+				$("<img/>").attr("src", item.media.m).addClass('flickr_img').appendTo("#images")
 					.wrap("<a href='" + item.link + "'></a>");
+					alert(item.title);
 			});
 		
-			$("#title").html(data.title);
 			$("#description").html(data.description);
-			$("#link").html("<a href='"+data.link+"' target=\"_blank\">Visit the Viget Inspiration Pool!</a>");
+			$("#link").html("<a href='"+data.link+"' target=\"_blank\">Visit the album</a>");
 				//Notice that the object here is "data" because that information sits outside of "items" in the JSON feed
 		
-			$('#images').cycle({
+/*			$('#images').cycle({
 				fx:     'fade',
 				speed:    'normal',
 				timeout:  0,
 				next:   '#next',
 				prev:   '#prev'
-			});
+			});*/
+			
 		});
 	
 
 });
 
 </script>
+
+<style>
+
+#images {
+	clear:both;
+	width: 800px;
+	margin: 0px auto;
+	border: thin solid pink;
+	min-height: 500px;
+}
+
+img.flickr_img {
+	margin: 0 .4em;
+	height: 220px;
+}
+
+</style>
 
 <div id="container">
 
@@ -44,12 +62,11 @@ $(function() {
 
 
 <div id="flickrNav"> 
-    <a id="prev" href="#">Prev</a><a id="next" href="#">Next</a>
+    <a id="prev" href="#">&laquo;&nbsp;Prev</a>&nbsp;<a id="next" href="#">Next&nbsp;&raquo;</a>
 </div>
 
-<h1 id="title"></h1> 
+<p id="link"></p>
 <p id="description"></p> 
-<p id="link"></p> 
 <div id="images"></div>
 
 <? include('../include/footer.php') ?>
